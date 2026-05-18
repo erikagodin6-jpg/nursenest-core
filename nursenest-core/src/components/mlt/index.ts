551ac71d0e6e2442df3f74f649decb260ec057ef
erikagodin6-jpg <erikagodin6@gmail.com>
2026-05-18 12:44:38 -0400

    feat(mlt): add component barrel exports

---
+++ nursenest-core/src/components/mlt/index.ts
@@ -0,0 +1,4 @@
+export { MltSimulationCockpit, default as DefaultMltSimulationCockpit } from "./mlt-simulation-cockpit";
+export { MltSimulationCockpitDemo, default as DefaultMltSimulationCockpitDemo } from "./mlt-simulation-cockpit-demo";
+export { MltMorphologyDrillPanel, default as DefaultMltMorphologyDrillPanel } from "./mlt-morphology-drill-panel";
+export { MltMorphologyDrillDemo, default as DefaultMltMorphologyDrillDemo } from "./mlt-morphology-drill-demo";
